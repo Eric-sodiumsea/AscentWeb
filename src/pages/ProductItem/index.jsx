2da808279486cc9460b791da2ad3ba09833b6a24
Eric_sodiumsea <2719495583@qq.com>
2022-11-14 17:09:11 +0800

    完成了 <ProductsShow /> 商品浏览 & 查询的前端部分；完成了 <ProductItem /> 商品详情的前端部分；修改了 <ProductShow /> 的布局(Row, Col)；完善了 <ProductEdit /> 商品编辑的上传图片部分(ImgCrop & Upload)和商品分类部分(Tag)；完善了 <UserEdit /> 用户编辑的分配商品部分(Tag)。

--- src/pages/ProductItem/index.jsx
+++ src/pages/ProductItem/index.jsx
@@ -1,7 +1,62 @@
 import React from 'react'
+import { Card, Image, Row, Col, Tag, InputNumber, Button } from 'antd';
+import { ShoppingCartOutlined, ShoppingOutlined } from '@ant-design/icons';
+import logo from '../../components/Frame/logo.png'
+
+const product = {
+    key: '1',
+    image: logo,
+    productName: 'A',
+    price: 32,
+    productNumber: 10,
+    tags: ['nice', 'developer'],
+    description: 'New York No. 1 Lake Park 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+}
 
 export default function ProductItem() {
     return (
-        <div>商品详情</div>
+        <Card
+            title="商品详情"
+            bordered={false}
+            hoverable={true}
+            headStyle={{ textAlign: 'center', fontWeight: 'bold' }}
+        >
+            <Row gutter={[24]}>
+                <Col span={8}>
+                    <Image src={logo} />
+                </Col>
+                <Col span={16}>
+                    <Row gutter={[24, 16]}>
+                        <Col span={3}>商品名：</Col>
+                        <Col span={21} style={{ fontWeight: 'bold' }}>{product.productName}</Col>
+                        <Col span={3}>商品价格：</Col>
+                        <Col span={21}>{product.price}</Col>
+                        <Col span={3}>商品分类：</Col>
+                        <Col span={21}>
+                            {
+                                product.tags.map(tag => {
+                                    return (
+                                        <Tag key={tag}>{tag}</Tag>
+                                    )
+                                })
+                            }
+                        </Col>
+                        <Col span={3}>详细描述：</Col>
+                        <Col span={21}><span style={{
+                            'wordWrap': 'break-word',
+                            'wordBreak': 'break-all',
+                            'overflow': 'hidden'
+                        }}>{product.description}</span></Col>
+                        <Col span={3}>数量：</Col>
+                        <Col span={21}><InputNumber min={1} max={product.productNumber} defaultValue={1} /></Col>
+                        <Col span={3}>仅剩：</Col>
+                        <Col span={21}>{product.productNumber}</Col>
+                        <Col span={3}><Button icon={<ShoppingCartOutlined />}>加入购物车</Button></Col>
+                        <Col span={3} offset={1}><Button type='primary' icon={<ShoppingOutlined />}>立即购买</Button></Col>
+                        <Col span={17}></Col>
+                    </Row>
+                </Col>
+            </Row >
+        </Card >
     )
 }
--- src/pages/ProductsShow/index.jsx
+++ src/pages/ProductsShow/index.jsx
@@ -1,7 +1,136 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { Card, Form, Select, Input, Button, Row, Col } from 'antd';
+import logo from '../../components/Frame/logo.png'
+import './index.css'
+const { Option } = Select;
+
+const products = [
+    {
+        productId: 1,
+        image: logo,
+        productName: "a",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 33456.15
+    },
+    {
+        productId: 2,
+        image: logo,
+        productName: "b",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 456.15
+    },
+    {
+        productId: 3,
+        image: logo,
+        productName: "c",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 123.15
+    },
+    {
+        productId: 4,
+        image: logo,
+        productName: "d",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+    {
+        productId: 5,
+        image: logo,
+        productName: "e",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+    {
+        productId: 6,
+        image: logo,
+        productName: "f",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+    {
+        productId: 7,
+        image: logo,
+        productName: "g",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+    {
+        productId: 8,
+        image: logo,
+        productName: "h",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+    {
+        productId: 9,
+        image: logo,
+        productName: "i",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+    {
+        productId: 10,
+        image: logo,
+        productName: "j",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        price: 789.50
+    },
+]
 
 export default function ProductsShow() {
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+
     return (
-        <div>商品浏览</div>
+        <Card
+            title='商品浏览'
+            headStyle={{ fontWeight: 'bold' }}
+            extra={
+                <Form
+                    name='search'
+                    layout='inline'
+                    onFinish={onFinish}
+                    initialValues={{ options: 'productName' }}
+                >
+                    <Form.Item name='options'>
+                        <Select style={{ width: '150px' }}>
+                            <Option value='productName'>按商品名查询</Option>
+                            <Option value='productCategory'>按商品种类查询</Option>
+                            <Option value='all'>查询所有</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item name='search'>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type='primary' htmlType='submit'>
+                            查询
+                        </Button>
+                    </Form.Item>
+                </Form>
+            }
+        >
+            <Row gutter={[24, 24]}>
+                {
+                    products.map(product => {
+                        return (
+                            <Col key={product.productId} className="gutter-row product" span={6}>
+                                <NavLink to={"detail/" + product.productId}>
+                                    <Card title={product.productName} headStyle={{ fontWeight: 'bold' }} hoverable>
+                                        <img src={logo} alt="样品图" />
+                                        <p className='price'>{product.price}</p>
+                                        <p className='description'>{product.description}</p>
+                                    </Card>
+                                </NavLink>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+        </Card>
     )
 }
