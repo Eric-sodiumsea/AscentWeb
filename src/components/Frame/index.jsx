4396f6a746bcf324158c4308f5a9c80a9643f9de
佰巛 <76035116+Eric-sodiumsea@users.noreply.github.com>
2022-11-23 10:15:45 +0800

    update Frame nickname

--- src/components/Frame/index.jsx
+++ src/components/Frame/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import { Layout, Menu, Dropdown, Space, Avatar } from 'antd'
 import { DownOutlined } from '@ant-design/icons';
@@ -6,31 +6,20 @@ import { adminRoutes, userRoutes } from '../../routes'
 import { useNavigate } from 'react-router-dom'
 import logo from './logo.png'
 import './index.css'
-import { removeToken, isLogined } from '../../utils/auth'
-
-const items = [
-    {
-        label: (<a href="/">修改账号信息</a>),
-        key: '0',
-    },
-    {
-        type: 'divider',
-    },
-    {
-        label: (
-            <NavLink to="/signin" onClick={removeToken}>退出登录</NavLink>
-        ),
-        key: '1',
-    },
-];
+import { getToken, removeToken, isLogined } from '../../utils/auth'
+import axios from '../../utils/axios';
 
 export default function Frame(props) {
     const { Header, Content, Sider } = Layout;
+
+    let navigate = new useNavigate();
+
+    // 侧边栏
     let routes;
     if (props.url === "admin") {
         routes = adminRoutes;
     } else {
-        routes = userRoutes
+        routes = userRoutes;
     }
     const isShowRoutes = routes.filter(route => route.isShow);
     const SidebarItems = isShowRoutes.map(route => {
@@ -41,14 +30,54 @@ export default function Frame(props) {
         }
     })
 
-    let navigate = new useNavigate();
-
-    // 判断是否登录，若还没登录，则重定向回登录页
+    // 判断当前账户是管理员还是普通用户
+    const [superuser, setSuperuser] = useState("");
+    // 记录用户名
+    const [username, setUsername] = useState("");
     useEffect(() => {
+        // 判断是否登录，若还没登录，则重定向回登录页
         if (!isLogined()) {
             navigate('/signin');
+        } else {
+            // 若已经登录，则判断该用户是管理员还是普通用户
+            axios.get('/user?methodName=findUserById&id=' + getToken())
+                .then(res => {
+                    setUsername(res.data.username);
+                    if (res.data.superuser === "2" || res.data.superuser === "3") {
+                        // 该用户为管理员，在前台页面时有权访问后台页面
+                        if (props.url === "user") {
+                            routes[6].isShow = true;
+                        }
+                        setSuperuser("admin");
+                    } else if (res.data.superuser === "1") {
+                        // 该用户为普通用户，无权访问后台页面，需跳转回前台页面
+                        if (props.url === "admin") {
+                            navigate('/user/products');
+                        }
+                        setSuperuser("user");
+                    }
+                }).catch(err => {
+                    console.log(err);
+                })
         }
-    }, [])
+    }, []);
+
+
+    const items = [
+        {
+            label: (<NavLink to={superuser === "admin" ? "/admin/users/editpower/" + getToken() : "/user/edit"} >修改账号信息</NavLink>),
+            key: '0',
+        },
+        {
+            type: 'divider',
+        },
+        {
+            label: (
+                <NavLink to="/signin" onClick={removeToken}>退出登录</NavLink>
+            ),
+            key: '1',
+        },
+    ];
 
     return (
         <Layout>
@@ -59,10 +88,10 @@ export default function Frame(props) {
                 <Dropdown
                     menu={{ items }}
                 >
-                    <NavLink to={"/admin/users/edit/" + isLogined()}>
+                    <NavLink to={superuser === "admin" ? "/admin/users/editpower/" + getToken() : "/user/edit"}>
                         <Space>
                             <Avatar>A</Avatar>
-                            <span style={{ color: 'white' }}>用户名</span>
+                            <span style={{ color: 'white' }}>{username}</span>
                             <DownOutlined style={{ color: 'white' }} />
                         </Space>
                     </NavLink>
