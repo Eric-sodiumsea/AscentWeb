49878361a728c21965370fe67b76121d98fc6334
Eric_sodiumsea <2719495583@qq.com>
2022-12-05 19:31:50 +0800

    fix: 修改了列宽

--- src/pages/ProductItem/index.jsx
+++ src/pages/ProductItem/index.jsx
@@ -9,33 +9,24 @@ import { ShoppingCartOutlined, ShoppingOutlined } from '@ant-design/icons';
 import logo from '../../components/Frame/logo.png'
 import axios from '../../utils/axios';
 
-// const product = {
-//     key: '1',
-//     image: logo,
-//     productName: 'A',
-//     price: 32,
-//     productNumber: 10,
-//     tags: ['nice', 'developer'],
-//     description: 'New York No. 1 Lake Park 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
-// }
-
 export default function ProductItem() {
     const [product, setProduct] = useState({ tags: [] });
     const { productId } = useParams();
     useEffect(() => {
         axios.get('/product?methodName=findProductById&id=' + productId)
             .then((res) => {
+                console.log("商品列表为：", res.data);
                 setProduct({
                     key: productId,
                     image: logo,
                     productName: res.data.productname,
                     price: res.data.price,
-                    productNumber: res.data.stock,
+                    stock: res.data.stock,
                     tags: res.data.categoryList.map(category => {
                         return category.categoryname;
                     }),
                     description: res.data.description,
-                })
+                });
             }).catch(error => {
                 console.log(error);
             })
@@ -57,12 +48,12 @@ export default function ProductItem() {
                 </Col>
                 <Col span={14}>
                     <Row gutter={[24, 16]}>
-                        <Col span={3}>商品名：</Col>
-                        <Col span={21} style={{ fontWeight: 'bold' }}>{product.productName}</Col>
-                        <Col span={3}>商品价格：</Col>
-                        <Col span={21}>{product.price}</Col>
-                        <Col span={3}>商品分类：</Col>
-                        <Col span={21}>
+                        <Col span={4}>商品名：</Col>
+                        <Col span={20} style={{ fontWeight: 'bold' }}>{product.productName}</Col>
+                        <Col span={4}>商品价格：</Col>
+                        <Col span={20}>{product.price}</Col>
+                        <Col span={4}>商品分类：</Col>
+                        <Col span={20}>
                             {
                                 product.tags.map(tag => {
                                     return (
@@ -71,19 +62,19 @@ export default function ProductItem() {
                                 })
                             }
                         </Col>
-                        <Col span={3}>详细描述：</Col>
-                        <Col span={21}><span style={{
+                        <Col span={4}>详细描述：</Col>
+                        <Col span={20}><span style={{
                             'wordWrap': 'break-word',
                             'wordBreak': 'break-all',
                             'overflow': 'hidden'
                         }}>{product.description}</span></Col>
-                        <Col span={3}>数量：</Col>
-                        <Col span={21}><InputNumber min={1} max={product.productNumber} defaultValue={1} /></Col>
-                        <Col span={3}>仅剩：</Col>
-                        <Col span={21}>{product.productNumber}</Col>
-                        <Col span={3}><Button icon={<ShoppingCartOutlined />} onClick={() => navigate('/user/cart')}>加入购物车</Button></Col>
-                        <Col span={3} offset={2}><Button type='primary' icon={<ShoppingOutlined />} onClick={() => navigate('/user/orders')}>立即购买</Button></Col>
-                        <Col span={16}></Col>
+                        <Col span={4}>数量：</Col>
+                        <Col span={20}><InputNumber min={1} max={product.stock} defaultValue={1} /></Col>
+                        <Col span={4}>仅剩：</Col>
+                        <Col span={20}>{product.stock}</Col>
+                        <Col span={4}><Button icon={<ShoppingCartOutlined />} onClick={() => navigate('/user/cart')}>加入购物车</Button></Col>
+                        <Col span={4} offset={1}><Button type='primary' icon={<ShoppingOutlined />} onClick={() => navigate('/user/orders')}>立即购买</Button></Col>
+                        <Col span={15}></Col>
                     </Row>
                 </Col>
             </Row >
