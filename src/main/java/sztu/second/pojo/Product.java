ded420923bc10c633109a9e0c8f25be8fd5d5bbc
Eric_sodiumsea <2719495583@qq.com>
2022-12-05 19:41:47 +0800

    fix: 修改了属性（stock 库存量）

--- src/main/java/sztu/second/pojo/Product.java
+++ src/main/java/sztu/second/pojo/Product.java
@@ -14,21 +14,17 @@ public class Product {
 
     private Integer id; //商品ID
 
-    private String productnumber; //商品数量
+    private String image; //商品图片
 
     private String productname; //商品名称
 
-    private String image; //商品图片
-
     private double price; //价格
 
-    private String stock; //库存量
-
-    private String delsoft; // 软删除标志 1 为软删除 0为正常
+    private Integer stock; //库存量
 
     private String description; // 商品描述
 
-    private String status;  // 商品上架状态
+    private String delsoft; // 软删除标志 1 为软删除 0为正常
 
     private List<Category> categoryList;    // 分类标签集合
 }
